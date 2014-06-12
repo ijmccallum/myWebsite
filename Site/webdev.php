6a1ded4dcac78aac8eb2471a6029589a12b84bce
ijmccallum <ijmccallum@hotmail.co.uk>
2014-06-11 21:42:05 -0400

    Web dev page update

--- Site/webdev.php
+++ Site/webdev.php
@@ -4,46 +4,66 @@
 <div class="row">
     <div class="col-md-6">
         <p class="lead">
+            I focus on building websites using the fantastic, and open source CMS, WordPress.  This allows me to set up sites very quickely and extend them to do almost anything you can imagine.  It is also a wonderfully simple system that lets you manage your content and even edit the structure of your site!
+        </p> <!-- if you would like your very own, shiney new web site, have a look at this questionaire.  It askes you all the questions that you and I need to know before starting. -->
+        <hr />
+    </div> <!-- END 6 column for text -->
+</div> <!-- END intro row -->
+            
+<div class="row">
+    <div class="col-md-6">
+    	<h3>Some WordPress examples:</h3>
+        <br />
+        <p><strong>Christina D Gonzalez</strong> is a Landscape Architect currently based in Washington DC.  This is her personal portfolio site and blog.</p>
+        <code><a target="_blank" href="http://cdgonzalez.com/">www.cdgonzalez.com</a></code>
+        <hr />
+        <p><strong>Wikilogic</strong> is a crowd-sourced logic mapping project.  It is a massive undertaking which, if successfull, has tremendous potential to benefit a huge number of people. For an overview of my invlovment it has it's own <a href="http://iainjmccallum.com/wikilogic.php">section</a> here on my site!  To find out more about the project, what it is, and how to get involved yourself - have a look at the home page:</p>
+        <code><a target="_blank" href="http://www.wikilogicfoundation.org/">www.WikiLogicFoundation.com</a></code>
+        <hr />        
+        <p><strong>Working Title</strong> is a UK based 3 piece band.  They needed a very simple, very quick site to list their various gigs and create an online presence.</p>
+        <code><a target="_blank" href="http://workingtitletheband.co.uk/">www.workingtitletheband.co.uk</a></code>
+        <hr />
+        <br />
+        <h3>Custom, Non-WordPress sites</h3>
+        <br />
+        <p><strong>Sencha Insights</strong> is a tech blog focusing on Sencha and related web technologies.  I built this site on Node.JS using the still-in-development Keystone.js CMS.  As these technologies are still fairly new the choice of templates is limited / non-existant.  I took a theme without any back end and threaded it into Keystone to create a site which is effectivly a very fancy blog.  It has it's own browser plugin which allows interesting articles to be saved whenever they pop up and it publishes it's newsletter automatically every two weeks.  This site practically runs it's self!</p>
+        <code><a target="_blank" href="http://www.senchainsights.com/">www.senchainsights.com</a></code>
+        <hr />
+        <p><strong>Sophie's Games</strong> is the group behind <a href="http://iainjmccallum.com/synergy.php">Synergy</a>.  We were brought together by a common interest in using games to create a positive impact or inspire thought on important issues.  The website is ment as a simple, clean area that describes us and points to our games without any extra fuss.</p>
+        <code><a target="_blank" href="http://sophiesgames.com/">www.sophiesgames.com</a></code>
+        <hr />
+    <!--     <p>Description of SwarmOnline</p>
+        <code><a target="_blank" href="http://www.swarmonline.com/">www.swarmonline.com</a></code>
+        <hr /> -->
+        <p><strong>Fuel Jack</strong> is a location, fuel, and theft tracking product aimed at companies with large vehicle fleets.  This brochure site was designed and built from scratch to give them a place to direct potentially interested customers.</p>
+        <code><a target="_blank" href="http://www.fueljack.com/">www.fueljack.com</a></code>
+        <hr />
+        <p><strong>Wedding site</strong>, a simple, minimal, statment of design.  As this is our own I was able to create it explicitly to my own taste and desire for simplicity.</p>
+        <code><a target="_blank" href="">www.iainandchrissy.com</a></code>
+        <hr />
+        <p>And a few more that I have worked on, usually in upkeep, updates, editing, general things really.</p>
+        <code><a target="_blank" href="http://coiaandassociates.com/">www.coiaandassociates.com</a></code><br />
+        <code><a target="_blank" href="http://www.drymenselfcatering.com/">www.drymenselfcatering.com</a></code><br />
+        <code><a target="_blank" href="http://www.swarmonline.com/">www.swarmonline.com</a></code>
+        <hr />
+        <p>Finally, for a few extra brownie points, here's a plugin I wrote for WordPress (to allow visitors to switch between different themes on a WordPress website):</p>
+        <code><a target="_blank" href="http://wordpress.org/plugins/ijm-theme-bar/">Wordpress Theme Switcher</a></code><br />
+        <hr />
+		<br />
+    </div> <!-- END column -->
+</div> <!-- END intro row -->
+
+<div class="row">
+    <div class="col-md-6">
+        <p class="lead">
             My interest in code was sparked while working on the art and design for <a href="http://iainjmccallum.com/synergy.php">Synergy</a>, 
             and I couldn't be more glad that it happened!  I learnt by working through the whole of Stanford's online 'Programming methodology' cource and getting stuck into development for WordPress themes and plugins using simple HTML, CSS and PHP.  
             Web Design and Development became a part of my freelance work which led to my first job as a Designer/Developer with a Software Development company where I picked up a far more in-depth understanding of development and the many tools that go with it.
-        	From then until now I have built and helped out with a number of projects, below are some of the more interesting ones.
+        	From then until now I have built and helped out with a number of projects, the list above shows some of the more interesting ones.
         </p>
         <hr />
     </div> <!-- END 6 column for text -->
 </div> <!-- END intro row -->
-<div class="col-md-6">
-	<p><strong>Wikilogic</strong> is a crowd-sourced logic mapping project.  It is a massive undertaking which, if successfull, has tremendous potential to benefit a huge number of people. For an overview of my invlovment it has it's own <a href="http://iainjmccallum.com/wikilogic.php">section</a> here on my site!  To find out more about the project, what it is, and how to get involved yourself - have a look at the home page:</p>
-    <code><a target="_blank" href="http://www.wikilogicfoundation.org/">www.WikiLogicFoundation.com</a></code>
-    <hr />
-	<p><strong>Sophie's Games</strong> is the group behind <a href="http://iainjmccallum.com/synergy.php">Synergy</a>.  We were brought together by a common interest in using games to create a positive impact or inspire thought on important issues.  The website is ment as a simple, clean area that describes us and points to our games without any extra fuss.</p>
-	<code><a target="_blank" href="http://sophiesgames.com/">www.sophiesgames.com</a></code>
-	<hr />
-    <p><strong>Christina D Gonzalez</strong> is a Landscape Architect for whome I put together this simple WordPress theme.  As it was one of the earliest website projects I worked on, this theme was built from scratch.</p>
-    <code><a target="_blank" href="http://cdgonzalez.com/">www.cdgonzalez.com</a></code>
-    <hr />
-    <p><strong>Fuel Jack</strong> is a location, fuel, and theft tracking product aimed at companies with large vehicle fleets.  This brochure site was designed and built from scratch to give them a place to refrence potentially interested customers.</p>
-    <code><a target="_blank" href="http://www.fueljack.com/">www.fueljack.com</a></code>
-    <hr />
-    <p><strong>Sencha Insights</strong> is a tech blog focusing on Sencha and related web technologies.  I built this site on Node.JS using the still-in-development Keystone.js CMS.  As these technologies are still fairly new the choice of templates is limited / non-existant.  I took a theme without any back end and threaded it into Keystone to create a site which is effectivly a very fancy blog.</p>
-    <code><a target="_blank" href="http://www.senchainsights.com/">www.senchainsights.com</a></code>
-    <hr />
-<!--     <p>Description of SwarmOnline</p>
-    <code><a target="_blank" href="http://www.swarmonline.com/">www.swarmonline.com</a></code>
-    <hr /> -->
-    <p><strong>Wedding site</strong>, a simple, minimal, statment of design.  As this is our own I was able to create it explicitly to my own taste and desire for simplicity.</p>
-    <code><a target="_blank" href="">www.iainandchrissy.com</a></code>
-    <hr />
-    <p>And a few more that I have worked on, usually in upkeep, updates, editing, general things really.</p>
-    <code><a target="_blank" href="http://coiaandassociates.com/">www.coiaandassociates.com</a></code><br />
-    <code><a target="_blank" href="http://www.workingtitletheband.co.uk/">www.workingtitletheband.co.uk</a></code><br />
-    <code><a target="_blank" href="http://www.drymenselfcatering.com/">www.drymenselfcatering.com</a></code><br />
-    <code><a target="_blank" href="http://www.swarmonline.com/">www.swarmonline.com</a></code>
-    <hr />
-    <p>Finally, for a few extra brownie points, here's a plugin I wrote for WordPress (to allow visitors to switch between different themes on a WordPress website):</p>
-    <code><a target="_blank" href="http://wordpress.org/plugins/ijm-theme-bar/">Wordpress Theme Switcher</a></code><br />
-    
-</div> <!-- END column -->
 
 
                 
