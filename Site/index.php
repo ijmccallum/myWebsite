e43be548ce31acb39cbaf7f599d750165608f876
ijmccallum <ijmccallum@hotmail.co.uk>
2014-07-20 20:54:17 -0400

    Tidy

--- Site/index.php
+++ Site/index.php
@@ -3,24 +3,26 @@
             
 	<div class="row">
 	    <div class="col-md-12">
-	        <p class="lead">Hello and welcome to my site!  As you may have guessed, I’m Iain.  I’ve developed a love of learning (everything) and am fascinated with The Internet.  I have built websites, developed video games, and created media for almost every form of digital design there is.  I also believe in simplicity.  Not just in it being the ultimate sophistication, but in it being a necessity for a happy life – which is my biggest aim!  And I have a sneaking suspicion we may have that aim in common.  So drop me a line, I’d love to chat.</p>
+	        <p class="lead">Hello and welcome to my site!  As you may have guessed, I’m Iain.  I’ve developed a love of learning (everything) and am fascinated with The Internet.  I have built websites, developed video games, and created media for almost every form of digital design there is.  I also believe in simplicity.  Not just in it being the ultimate sophistication, but in it being a necessity for a happy life – which is my biggest aim!  And I have a sneaking suspicion we may have that aim in common.  So drop me a line, I’d love to chat: <code style="font-size:70%;">IJMcCallum {at} hotmail {dot} co {dot} uk </code></p>
 	    </div> <!-- END 6 column for text -->
 	</div> <!-- END intro row -->
+	<!--
 	<div class="row">
 		<div class="col-md-6">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-			    	<h3 class="panel-title">Hacker News</h3>
+			    	<h3 class="panel-title">WordPress Chat</h3>
 				</div>
 			  	<div class="panel-body">
-			    	Panel content
+			    	<a class="twitter-timeline" href="https://twitter.com/search?q=%23wordpress+OR+%23Wordcamp" data-widget-id="491020932677259264">Tweets about "#wordpress OR #Wordcamp"</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			  	</div>
 			</div>
 		</div>
 		<div class="col-md-6">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-			    	<h3 class="panel-title">Happening in the Twittersphere</h3>
+			    	<h3 class="panel-title">HTML5, Javascript, CSS3, NodeJS</h3>
 				</div>
 			  	<div class="panel-body">
 			    	<a class="twitter-timeline" href="https://twitter.com/search?q=%23HTML5+OR+%23Javascript+OR+%23CSS+OR+%23DCTech" data-widget-id="482636761500094464">Tweets about "#HTML5 OR #Javascript OR #CSS OR #DCTech"</a>
@@ -29,12 +31,8 @@
 			</div>
 		</div>
     </div>
+-->
 
-
-
-    <div class="col-md-6 text-center">
-        <code>IJMcCallum {at} hotmail {dot} co {dot} uk </code>
-    </div> <!-- END column -->
 
 
                 
