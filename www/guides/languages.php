22826b78e2986f317db0e1c775f535740993b262
Iain J McCallum <ijmccallum@hotmail.co.uk>
2014-09-15 11:31:17 -0400

    Testing section added

--- www/guides/languages.php
+++ www/guides/languages.php
@@ -109,5 +109,16 @@
 <h4>Single Page Application (SPA) - is this a pattern?</h4>
 <p>Initially load a shell page, the user navigates through views loaded into that shell - no reloading.</p>
                 
+
+<hr />
+
+<h2>Testing</h2>
+
+<h4>End to End testing</h4>
+
+<h4>Unit testing</h4>
+
+<hr />
+
 <?php $footerAddress = (ltrim($homePath,'"')) . 'partials/footer.php'; ?>
 <?php include $footerAddress; ?>
