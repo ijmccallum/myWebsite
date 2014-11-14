0d61aabb87960a96d9b059a543132f175dcf5b1a
Iain J McCallum <ijmccallum@hotmail.co.uk>
2014-11-14 13:41:00 -0500

    investnig

---
+++ www/notes/investing.php
@@ -0,0 +1,22 @@
+<?php $iainPageTitle = 'Investing'; $docDepth = 1;?>
+<?php include '../partials/head.php'; ?>
+
+
+<h3>Stocks / shares</h3>
+
+<ul>
+	<li><strong>Stock: </strong> </li>
+	<li><strong>Share: </strong> </li>
+	<li><strong>Bond: </strong> </li>
+</ul>
+
+<hr />
+
+<h3>Trading</h3>
+<ul>
+	<li>Forex</li>
+	<li>Futures</li>
+</ul>
+
+<?php $footerAddress = (ltrim($homePath,'"')) . 'partials/footer.php'; ?>
+<?php include $footerAddress; ?>
