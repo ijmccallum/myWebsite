2c8080378996e6ba8d448237a528a43c1e5f34a7
Iain J McCallum <ijmccallum@hotmail.co.uk>
2014-11-14 13:40:02 -0500

    Wordpress notes

--- www/notes/wordpress.php
+++ www/notes/wordpress.php
@@ -18,8 +18,21 @@
 <p>We know it, a simple, easy to use blogging solution, not as powerful as Drupal or Joomla but so many extensions!  Limited design flexibility.  Good at multiple users</p>
 
 
+<hr />
+<p>WordPress as an operating system for the web</p>
 
-
+<p>I would like to see a few things / code a few plugins / find out if they already exist somewhere:
+<ul>
+	<li>A better code editor</li>
+	<li>A file system explorer for the server</li>
+	<li>A terminal</li>
+</ul>
+</p>
+<p>For me to do to mine:
+<ul>
+	<li>My dashboard my home</li>
+</ul>
+</p>
                 
 <?php $footerAddress = (ltrim($homePath,'"')) . 'partials/footer.php'; ?>
 <?php include $footerAddress; ?>
