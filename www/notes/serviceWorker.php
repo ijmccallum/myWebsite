ecf1d038f017da39f94f7eeeaaaf30357cbc6534
Iain J McCallum <ijmccallum@hotmail.co.uk>
2014-11-19 21:06:38 -0500

    service worker page added - notes to come!

---
+++ www/notes/serviceWorker.php
@@ -0,0 +1,8 @@
+<?php $iainPageTitle = 'HTTP'; $docDepth = 1;?>
+<?php include '../partials/head.php'; ?>
+
+
+
+
+<?php $footerAddress = (ltrim($homePath,'"')) . 'partials/footer.php'; ?>
+<?php include $footerAddress; ?>
