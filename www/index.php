c239fe7dad01558f3381510b6f160cd33af671fb
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-01-29 21:12:17 -0500

    A new me!

--- www/index.php
+++ www/index.php
@@ -1,17 +1,19 @@
 <?php $iainPageTitle = '@ijmccallum'; $docDepth = 0;?>
 <?php include 'partials/head.php'; ?>
-	
+
 	<div class="row">
-		<div class="col-md-4">
-			<p>Pic of me</p>
+		<div class="col-md-4" style="text-align:center;">
+			<p>
+				<img src="pics/me8.jpg" alt="" />
+			</p>
 		</div>
 		<div class="col-md-8">
 			<p class="text-col-2">
-				I can't stop learning things, it's brilliant! Curiosity took a bit of a hit throughout the education phase of life 
-				but it's recovered now, everything is fascinating again! For me, out of anything, the <i>most</i> interesting thing is the 
+				I can't stop learning things, it's brilliant! Curiosity took a bit of a hit throughout the education phase of life
+				but it's recovered now, everything is fascinating again! For me, out of anything, the <i>most</i> interesting thing is the
 				internet - not just designing websites or building apps, but the potential they have to impact people, it's phenomenal.<br />
-				Took me a while to figure out this passion, but since it dawned on me I've been set on learning everything there is to 
-				learn about the web, from networking protocols to the weirdest trends/projects floating around. “Find something you love to do, 
+				Took me a while to figure out this passion, but since it dawned on me I've been set on learning everything there is to
+				learn about the web, from networking protocols to the weirdest trends/projects floating around. “Find something you love to do,
 				and you'll never work a day in your life” - yep, I never used to believe in that!<br />
 			</p>
 		</div>
@@ -20,8 +22,8 @@
 	<div class="row">
 		<div class="col-md-4">
 			<img src="pics/webDev/wikilogic02.jpg" class="webDevScreenshot">
-			<p><strong>Wikilogic</strong> is an open source project currently being developed and looked after by myself and a few others.  
-	            The idea is to create a system that will allow us to map out all logic, or as much of it as we can fit into a sharded database.  
+			<p><strong>Wikilogic</strong> is an open source project currently being developed and looked after by myself and a few others.
+	            The idea is to create a system that will allow us to map out all logic, or as much of it as we can fit into a sharded database.
 	            This might seem a little wild at a holistic level, but it's actually quite simple.<br /><br />  We have a live demo running which I can supply a link to on request,
 	            or you can get it running locally by visiting the <a href="https://github.com/WikiLogic/WikiLogic">Github</a> page.  We'll be writing about more general progress
 	            on the <a href="http://www.wikilogicfoundation.org/" target="_blank">main site</a> and the in depth theory is being laid out in our <a href="http://www.wikilogicfoundation.org/wiki">meta wiki</a>.
@@ -42,8 +44,8 @@
 		<div class="col-md-4">
 			<img src="pics/bubbles-shot.png" class="webDevScreenshot">
 			<p><strong>Bubbles!</strong> was an experiment in app development for Android.  It was only meant to be a test to run through the process of creating and publishing
-	            but it turned into a full game written with <code>Create.js</code> and wrapped with <code>Cordova</code>.  As a result of this being a small test that just kept growing, 
-	            it's all written in one gigantic javascript file!  Taught me a thing or two about what not to do.  Still, got it working, pretty proud of that!<br />  
+	            but it turned into a full game written with <code>Create.js</code> and wrapped with <code>Cordova</code>.  As a result of this being a small test that just kept growing,
+	            it's all written in one gigantic javascript file!  Taught me a thing or two about what not to do.  Still, got it working, pretty proud of that!<br />
 	            <br />
 	            You can now download it onto any android device or just play it in the browser.
 	        </p>
@@ -66,9 +68,9 @@
 			<h2><a href="webdev.php">Web App Development</a></h2>
 			<p><em>An interactive service accessible through any browser from any device.<br />
 				Intended as a tool as opposed to a resource.<br />
-				e.g., <a href="http://trello.com">Trello</a>, 
-				<a href="http://prezi.com/" target="_blank">Prezi</a>, 
-				<a href="http://c9.io/">Cloud9</a>, 
+				e.g., <a href="http://trello.com">Trello</a>,
+				<a href="http://prezi.com/" target="_blank">Prezi</a>,
+				<a href="http://c9.io/">Cloud9</a>,
 				<a href="http://grooveshark.com/">Grooveshark</a></em></p>
 			<br />
 			<div class="iconBox">
@@ -116,7 +118,7 @@
 				<p>KeystoneJS</p>
 			</div>
 			-->
-			
+
 		</div>
 	</div>
 
@@ -157,8 +159,8 @@
 				<li class="list-group-item">Visit the <a href="http://www.nationalweddingshow.co.uk/" target="_blank">National site</a><br />or the <a href="http://www.theukweddingshows.co.uk/" target="_blank">UK site</a></li>
 			</ul>
     	</div>
-    </div>    
-    -->    
+    </div>
+    -->
     <hr id="aboutMe" />
 	<div class="row" style="text-align:center;">
 		<h2 class="quirkyHeading">A bit about me</h2>
@@ -166,42 +168,42 @@
 	<div class="row">
 		<div class="col-md-2"></div>
 	    <div class="col-md-8">
-	    	<p class="text-col-2">How I got here is a bit of a story.  A series of discoveries that lead 
-	    		through more than a few creative industries.  The first happened while I was training 
-	    		to be an Architect in Edinburgh, Scotland.  I discovered just how much more amazing 
-	    		drafting and 3D software is in comparison to hand drawing.  It felt like discovering 
+	    	<p class="text-col-2">How I got here is a bit of a story.  A series of discoveries that lead
+	    		through more than a few creative industries.  The first happened while I was training
+	    		to be an Architect in Edinburgh, Scotland.  I discovered just how much more amazing
+	    		drafting and 3D software is in comparison to hand drawing.  It felt like discovering
 	    		the wheel!  <br /><br />
 
-	    		I'd had a taste of the digital world and wanted more, so I finished up my 
+	    		I'd had a taste of the digital world and wanted more, so I finished up my
 	    		training and moved to one of Scotland's technology hubs, the University of Abertay in Dundee.
-	    		I learnt modeling, texturing, rigging and animation in 3D, special effects with particle systems, 
-	    		lighting, stereoscopics, editing, screen writing, sound effects, and, to top it all off, 
+	    		I learnt modeling, texturing, rigging and animation in 3D, special effects with particle systems,
+	    		lighting, stereoscopics, editing, screen writing, sound effects, and, to top it all off,
 	    		I discovered that all this was being applied procedurally in game development.  <br /><br />
 
 	    		This blew my mind and I loved all of it, but you can't run down 10 different career paths and find success.
-	    		So to find my way, I began taking on freelance projects in a few of the different industries.  
+	    		So to find my way, I began taking on freelance projects in a few of the different industries.
 	    		I did some 3D modeling and animation for a couple of indi movies, some VFX filled motion graphics
 	    		for an advertising agency, some Graphic Design and some Illustration.  <br /><br />
 
-	    		I have always been described as a creative person so it was a little surprising that none of these options clicked, 
-	    		in the end it was while working on the game art for <a href="http://sophiesgames.com/">Synergy</a> with a group of indi developers when I 
+	    		I have always been described as a creative person so it was a little surprising that none of these options clicked,
+	    		in the end it was while working on the game art for <a href="http://sophiesgames.com/">Synergy</a> with a group of indi developers when I
 	    		figured out what did - programming!  Looking back across all the things I've gone through it suddenly
 	    		seems clear that this was the thing linking it all.  Underneath everything it was the software itself
 	    		that I was finding enthralling.  From there I've had a pretty focused drive into the world of Web Development
-	    		which brings us to now!<br /><br />  
+	    		which brings us to now!<br /><br />
 
 	    		If you've read this far, thank you, it's amazing you lasted!  We must have some pretty similar
 	    		interests - I want to hear from you so send me a hello: <code style="font-size:70%;">IJMcCallum {at} hotmail {dot} co {dot} uk </code>,
 	    		honestly, just say "Hi, we've got similar interests. All the best, &lt;your name&gt;".<br /><br />
 <!--
-	    		Stanford's online course in programming methodology got me started with the fundamentals. W3 Schools have done 
+	    		Stanford's online course in programming methodology got me started with the fundamentals. W3 Schools have done
 	    		a great job in laying out the details of HTML(5) and CSS(3).  The WordPress documentation is vast but persistance
 	    		allowed me to pick apart and start building themes.  I won a few freelance contracts then got hired in house to maintain
 	    		a collection of aging websites for a company that had switched to building apps with Sencha and NodeJS - while there I
 	    		picked up the fundamentals of ExpressJS, MongoDB, and some more in depth Javascript.  From there I immgrated accross the
-	    		Atlantic, discovered AngularJS and fell in love with it!  
+	    		Atlantic, discovered AngularJS and fell in love with it!
 -->
-	    	
+
 	        <!--
 	        <p class="lead">Hello and welcome to my site!  As you may have guessed, I’m Iain.  I’ve developed a love of learning (everything) and am fascinated with The Internet.  I have built websites, developed video games, and created media for almost every form of digital design there is.  I also believe in simplicity.  Not just in it being the ultimate sophistication, but in it being a necessity for a happy life – which is my biggest aim!  And I have a sneaking suspicion we may have that aim in common.  So drop me a line, I’d love to chat: <code style="font-size:70%;">IJMcCallum {at} hotmail {dot} co {dot} uk </code></p>
 	    	-->
@@ -253,11 +255,11 @@
 	    <div class="col-md-4">
 	        <h3>Graphic Design<br />& Advertising</h3>
 	    </div>
-	</div> 
+	</div>
 -->
 
 
 
 
-                
+
 <?php include 'partials/footer.php'; ?>
