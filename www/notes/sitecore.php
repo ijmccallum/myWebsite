1d952587f8b28965caff7bd36d4954f80eb9eed7
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-02-18 11:33:32 -0500

    Update sitecore.php

--- www/notes/sitecore.php
+++ www/notes/sitecore.php
@@ -3,10 +3,11 @@
 
 <h4>Overview</h4>
 <ul>
+  <li><strong>Item:</strong> </li>
+  <li><strong>Templates:</strong> Holds the html (and .net tags for other partials/fields)</li>
+  <li><strong>Fields:</strong> fields! .net style - Must be within a section within a template.</li>
   <li><strong>Layouts:</strong> each page has a layout file (.NET / .aspx)<br />
   <i>Layout files can have sub layouts (.ascx) and renderings (.xslt). So use a layout for the page outline</i></li>
-  <li><strong>Templates:</strong> two types? Presentation / data </li>
-  <li><strong>Item:</strong> </li>
   <li><strong>Rendering:</strong> Should be used for presenting content only</li>
   <li><strong>Sublayout:</strong> is a .Net web user control, free to do what you want</li>
 </ul>
